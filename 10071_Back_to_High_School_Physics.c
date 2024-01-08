d52b087e9bed822c3c48195e4b338eb05b51122c
Md. Mamun Miah <75275091+csemamun@users.noreply.github.com>
2024-01-08 14:18:03 +0600

    Add files via upload

---
+++ 10071_Back_to_High_School_Physics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,v;
+    while (scanf("%d%d",&a,&b)==2)
+    {
+        v = (a*b)*2;
+        printf("%d\n",v);
+
+    }
+
+    return 0;
+}
---
+++ 10110_Light,_more_light.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    long long int number;
+
+    while(scanf("%lld",&number) == 1)
+    {
+        if(number == 0)
+            break;
+        else
+        {
+            long long int temp = sqrt(number);
+            temp *=temp;
+            if(temp == number)
+                printf("yes\n");
+            else
+                printf("no\n");
+        }
+
+    }
+
+    return 0;
+}
